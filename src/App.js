6722515a5e939e4a6ee5b7d0789cc9c7a64ce646
tquangdang <thanhquangdang2002@gmail.com>
2023-12-31 09:57:18 -0600

    Add UpdatePlace

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 import UserPlaces from "./places/pages/UserPlaces";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import UpdatePlace from "./places/pages/UpdatePlace";
 const App = () => {
   return (
     <Router>
@@ -24,6 +25,9 @@ const App = () => {
           </Route>
           <Route path="/places/new" exact>
             <NewPlace />
+          </Route>
+          <Route path="/places/:placeId">
+            <UpdatePlace/>
           </Route>
           <Redirect to="/" />
         </Switch>
